50e49094348f40176df8ffc77fff53ca96a7671d
davidResca <davidresca9@gmail.com>
2022-05-31 19:33:39 -0300

    agrega menu ham

--- js/navbar.js
+++ js/navbar.js
@@ -1,3 +1,5 @@
 const burgerLogo = document.querySelector('#nav-burger-icon');
 const navWhatsappIcon = document.querySelector('#nav-whatsapp-icon');
 const navMenu = document.querySelector('.nav-menu');
+
+
